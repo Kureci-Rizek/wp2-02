d9c7fe2f7d3ffc7abe2d06a888ad881b901f3dee
kristianklimek <klimekkristian@gmail.com>
2018-10-09 11:04:54 +0200

    Dobrý dn, užijte si divadlo.

---
+++ index.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html lang="cs" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="styly/styly.css">
+    <meta charset="utf-8">
+    <title>kok</title>
+  </head>
+  <body>
+ Pohlaví:   <div>
+            <input type="radio" id="Muž"
+                   name="drone" value="muz" checked />
+            <label for="muž">Muž</label>
+        </div>
+
+        <div>
+            <input type="radio" id="žena"
+                   name="drone" value="žena" />
+            <label for="žena">Žena</label>
+        </div>
+Věk:
+<input type="number" id="věk"
+      name= "drone" value="Zadej věk" />
+      <label for="věk">Věk</label>
+
+ <br >Copyright (c) 2018 Copyright Holder All Rights Reserved.
+
+      </form>
+    </body>
+  </html>
